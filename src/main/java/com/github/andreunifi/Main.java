c9dc20ea7ba462b7dacb11e9195f94f7fe4858fe
andre <andrea.mugnaini3@stud.unifi.it>
2021-05-21 17:39:28 +0200

    Fixed Typo

--- src/main/java/com/github/andreunifi/Main.java
+++ src/main/java/com/github/andreunifi/Main.java
@@ -6,21 +6,23 @@ public class Main {
 
 
     public static void main(String[] args) throws InterruptedException {
-        Shared_Counter_Requester counter = new Shared_Counter_Requester();
-        Limited_Shared_Queue queue = new Limited_Shared_Queue(15);
+        Shared_Counter_Requester counter = new Shared_Counter_Requester(); //contatore condiviso
+        Limited_Shared_Queue queue = new Limited_Shared_Queue(15); //coda Request limitata
 
-        WorkerManager manager = new WorkerManager(20);
+        WorkerManager manager = new WorkerManager(20); //manager per i worker(size=20)
 
         for (int i = 0; i < 10; i++) {
             Requester requester = new Requester(queue, counter);
             requester.setName(String.valueOf(i));
             requester.start();
         }
+        //inizializzo i Requester
         for (int i = 0; i < 4; i++) {
             Assigner assigner = new Assigner(queue, manager);
             assigner.setName(String.valueOf(i));
             assigner.start();
         }
+        //Inizializzo gli Assigner
 
 
     }
